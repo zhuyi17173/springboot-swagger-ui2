3a40224ec6c5a01db60cfd3d14a5ab30b78e693a
e <QMTG012@VENUTECH.onaliyun.com>
2020-11-03 11:32:02 +0800

    第一次提交1.0

---
+++ src/main/java/com/example/springbootswaggerui2/SpringbootSwaggerUi2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootswaggerui2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSwaggerUi2Application {
+    //测试git提交
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSwaggerUi2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootswaggerui2/SpringbootSwaggerUi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootswaggerui2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSwaggerUi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
