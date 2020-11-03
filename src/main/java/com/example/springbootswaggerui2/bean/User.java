c0cc907e127b38ed35b794c78cdda9664649f7d2
e <QMTG012@VENUTECH.onaliyun.com>
2020-11-03 13:37:09 +0800

    第四次 age 提交

--- src/main/java/com/example/springbootswaggerui2/bean/User.java
+++ src/main/java/com/example/springbootswaggerui2/bean/User.java
@@ -4,6 +4,7 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private int age;
 
     public int getId() {
         return id;
@@ -27,6 +28,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     @Override
